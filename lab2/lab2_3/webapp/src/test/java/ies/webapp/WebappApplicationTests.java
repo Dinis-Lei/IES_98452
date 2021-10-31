1a63b46bceecf33fa2f98f34b19ceffc68c07b8f
Dinis-Lei <dinislei@ua.pt>
2021-10-31 16:52:31 +0000

    finished 2.3

---
+++ lab2/lab2_3/webapp/src/test/java/ies/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package ies.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
